c0b794e19215ce2ef21e6b149dd0867b9278cefe
Richie_Xue <rxue40@stuy.edu>
2023-12-02 13:14:19 -0500

    refactored code

--- 1/2023_1.c
+++ 1/2023_1.c
@@ -2,38 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 
-int findFirstNum(char buff[]) {
-        size_t i = 0;
-        while(buff[i] != '\0' && i >= 0) {
-            char c = buff[i];
-            if (c == '\0' || c == '\n' || c == '\r') {
-                break;  //reached end of line 
-            }
-             if (c > '0' && c <= '9') {
-                char* temp = &c;
-                //printf("The first num in %s is %d\n", string, num);
-                return c - 48;
-            }
-            i++;
-        }
-        return 0;
-    }
-
-int findLastNum(char buff[]) {
-    size_t i = strlen(buff) - 1;
-    while(buff[i] != '\0' && i >= 0) {
+int findNum(char buff[], int ifFirst) {
+    size_t i = ifFirst ? 0 : strlen(buff) - 1;
+    while (buff[i] != '\0' && i >= 0) {
         char c = buff[i];
-
         if (c > '0' && c <= '9') {
             char* temp = &c;
             //printf("The first num in %s is %d\n", string, num);
             return c - 48;
         }
-        i--;
+        if (ifFirst) {
+            i++;
+        } else {
+            i--;
+        }
     }
-    return 0;
 }
-
 
 int main(void) {
     // FILE* file = fopen("test.txt", "r");
@@ -42,12 +26,11 @@ int main(void) {
     char buff[255];
     int result = 0;
     while(fgets(buff, 255, fptr)) {
-        int firstNumber = findFirstNum(buff);
-        int lastNumber = findLastNum(buff);
+        int firstNumber = findNum(buff, 1);
+        int lastNumber = findNum(buff, 0);
         result += (firstNumber* 10) + lastNumber;
         //printf("\n%s [%d,%d]", buff, firstNumber, lastNumber);
     }
     fclose(fptr);
     printf("\n[Day 1 | Part 1] Solution: %d\n", result);
-
 }
